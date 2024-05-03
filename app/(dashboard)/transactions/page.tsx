808f9d951ef9f07ac74188777e90fb93b1017dd2
Sung Kil Han <sungkilhan221@gmail.com>
2024-05-03 18:40:48 -0400

    chore: Update DateInput component styles
    The commit updates the styles of the DateInput component by adding the `bg-transparent` class to the month, day, and year input fields. This change makes the input fields transparent, improving the visual appearance of the component.

---
+++ app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { MAX_DATE_RANGE_DAYS } from "@/lib/constants";
+import { differenceInDays, startOfMonth } from "date-fns";
+import { useState } from "react";
+import { toast } from "sonner";
+
+function TransactionsPage() {
+  const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
+    from: startOfMonth(new Date()),
+    to: new Date(),
+  });
+
+  return (
+    <div className="border-b bg-card">
+      <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+        <div>
+          <p className="text-3xl font-bold">Transactions history</p>
+        </div>
+        <DateRangePicker
+          initialDateFrom={dateRange.from}
+          initialDateTo={dateRange.to}
+          showCompare={false}
+          onUpdate={(value) => {
+            const { from, to } = value.range;
+            // We update the date range only if both dates are set
+
+            if (!from || !to) return;
+
+            if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+              toast.error(
+                `The selected date range is too long. Max allowed range is ${MAX_DATE_RANGE_DAYS} days`
+              );
+              return;
+            }
+
+            setDateRange({ from, to });
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default TransactionsPage;
