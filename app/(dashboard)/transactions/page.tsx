da674e81bfc56f9158467401d851988702df8c00
Sung Kil Han <sungkilhan221@gmail.com>
2024-05-03 22:20:36 -0400

    chore: Update DateInput component styles

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -5,6 +5,7 @@ import { MAX_DATE_RANGE_DAYS } from "@/lib/constants";
 import { differenceInDays, startOfMonth } from "date-fns";
 import { useState } from "react";
 import { toast } from "sonner";
+import TransactionTable from "./_components/TransactionTable";
 
 function TransactionsPage() {
   const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
@@ -13,33 +14,38 @@ function TransactionsPage() {
   });
 
   return (
-    <div className="border-b bg-card">
-      <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
-        <div>
-          <p className="text-3xl font-bold">Transactions history</p>
+    <>
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+          <div>
+            <p className="text-3xl font-bold">Transactions history</p>
+          </div>
+          <DateRangePicker
+            initialDateFrom={dateRange.from}
+            initialDateTo={dateRange.to}
+            showCompare={false}
+            onUpdate={(value) => {
+              const { from, to } = value.range;
+              // We update the date range only if both dates are set
+
+              if (!from || !to) return;
+
+              if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+                toast.error(
+                  `The selected date range is too long. Max allowed range is ${MAX_DATE_RANGE_DAYS} days`
+                );
+                return;
+              }
+
+              setDateRange({ from, to });
+            }}
+          />
         </div>
-        <DateRangePicker
-          initialDateFrom={dateRange.from}
-          initialDateTo={dateRange.to}
-          showCompare={false}
-          onUpdate={(value) => {
-            const { from, to } = value.range;
-            // We update the date range only if both dates are set
-
-            if (!from || !to) return;
-
-            if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
-              toast.error(
-                `The selected date range is too long. Max allowed range is ${MAX_DATE_RANGE_DAYS} days`
-              );
-              return;
-            }
-
-            setDateRange({ from, to });
-          }}
-        />
       </div>
-    </div>
+      <div className="container">
+        <TransactionTable from={dateRange.from} to={dateRange.to} />
+      </div>
+    </>
   );
 }
 
